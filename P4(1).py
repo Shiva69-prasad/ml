38a1a747b6103e2c62714330fcd5d79d2baf236a
Shiva69-prasad <shiva31694@gmail.com>
2025-06-09 07:12:04 +0530

    P4(1).py

---
+++ P4(1).py
@@ -0,0 +1,8 @@
+import csv
+def find_s(f):
+    d = list(csv.reader(open(f)))[1:]
+    h = d[0][:-1]
+    for r in d[1:]:
+        if r[-1].lower()=='yes': h = [x if x==y else '?' for x,y in zip(h,r[:-1])] # lower is a function use ()
+    return h
+print(find_s('training_data.csv'))
